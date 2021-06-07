7f11e7764fea5181e4a5febd46fcc80d95b6adac
Vikram Kumar <vikcanada90@gmail.com>
2021-06-07 03:35:32 -0400

    minor tweaks

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Show } from '../models/show.model';
+import {PouchService} from '../services/pouch.service';
 
 @Component({
   selector: 'app-watchlist',
@@ -6,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./watchlist.component.css']
 })
 export class WatchlistComponent implements OnInit {
-  constructor() { }
+  shows: Array<Show>;
+  constructor(private pouch: PouchService) {}
 
   ngOnInit(): void {
+    this.loadShows();
   }
 
+  async loadShows(): Promise<void> {
+    this.shows = await this.pouch.getAllShows();
+  }
 }
